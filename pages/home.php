a6a17c38c63ed92cf14a23d663926bd47234416e
Guilherme Tupinambá Durães <guilherme.tupinamba@gmail.com>
2020-09-21 01:39:56 -0300

    Home buttons

--- pages/home.php
+++ pages/home.php
@@ -11,9 +11,9 @@
     <div class="container">
         <h1 class="title">TETRIS UNICHAMPS</h1>
         <div class="containerButton">
-            <div class="buttons play"><a href="<?php echo INCLUDE_PATH; ?>play" class="button"><img src="<?php echo INCLUDE_PATH; ?>assets\icons\play.svg" /> JOGAR</a></div class="buttons">
-            <div class="buttons ranking"><a href="<?php echo INCLUDE_PATH; ?>ranking" class="button"><img src="<?php echo INCLUDE_PATH; ?>assets\icons\trophy.svg" /> RANKING</a></div class="buttons">
-            <div class="buttons signOut"><a href="http://localhost/tetris?logout" class="button"><img src="<?php echo INCLUDE_PATH; ?>assets\icons\signOut.svg" /> SAIR</a></div class="buttons">
+            <button class="buttons play" href="<?php echo INCLUDE_PATH; ?>play" class="button"><img src="<?php echo INCLUDE_PATH; ?>assets\icons\play.svg" />JOGAR</button>
+            <button class="buttons ranking" href="<?php echo INCLUDE_PATH; ?>ranking" class="button"><img src="<?php echo INCLUDE_PATH; ?>assets\icons\trophy.svg" />RANKING</button>
+            <button class="buttons signOut" href="http://localhost/tetris?logout" class="button"><img src="<?php echo INCLUDE_PATH; ?>assets\icons\signOut.svg" />SAIR</button>
         </div>
     </div>
 </section>
