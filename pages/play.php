a00034e93f8e60d6a4620ea19f9861c96ee39514
Guilherme Tupinambá Durães <guilherme.tupinamba@gmail.com>
2020-09-22 00:16:52 -0300

    Play

--- pages/play.php
+++ pages/play.php
@@ -1,11 +1,44 @@
 <style>
-    <?php include('styles/home.css'); ?>
+    <?php include('styles/play.css'); ?>
 </style>
 
 <section class="play">
     <div class="container">
-        <div class="info"></div>
+        <h1 class="title">TETRIS UNICHAMPS</h1>
+        <div class="info">
+            <div class="gameTime">
+                <h1>TIME:</h1>
+                <h2>00:30</h2>
+            </div>
+            <div class="score">
+                <h1>SCORE:</h1>
+                <h2>100</h2>
+            </div>
+            <div class="next">
+                <h1>PRÓXIMA PEÇA:</h1>
+                <h2>100</h2>
+            </div>
+        </div>
         <div class="game"></div>
-        <div class="rank"></div>
+        <div class="rank">
+            <h1>RANK</h1>
+            <div class="toggleRank">
+                <div class="geral">
+                    <h1>RANK GERAL</h1>
+                    <button>SWITCH</button>
+                </div>
+                <div class="personal">
+                    <h1>RANK GERAL</h1>
+                    <button>SWITCH</button>
+                </div>
+            </div>
+            <div class="rankTable">
+                <div class="row">
+                    <div class="col">Pos.</div>
+                    <div class="col">Player</div>
+                    <div class="col">Score</div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
