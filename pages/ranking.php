e2a4942d6a36b9631c606213ba559f4d7ea67f2f
Marcos Nocetti <work@marcosnocetti.com>
2020-09-21 22:06:19 -0300

    Column div add

--- pages/ranking.php
+++ pages/ranking.php
@@ -20,44 +20,92 @@
                 </div>
                 <div class="scoreTable">
                 <div class="scores">
-                        <h4 class="rank">1</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">100</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">2</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">98</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">3</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">97</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">4</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">90</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">5</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">100</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">6</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">98</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">7</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">97</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">8</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">90</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column"> 
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                 </div>
             </div>
@@ -65,50 +113,104 @@
             <div class="rankTable">
                 <h3 class="rankTitle">LOCAL RANK</h3>
                 <div class="scores">
-                    <h4 class="rank">Pos.</h4>
-                    <h4 class="player">Player</h4>
-                    <h4 class="score">Score</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                 </div>
                 <div class="scoreTable">
                     <div class="scores">
-                        <h4 class="rank">1</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">100</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">2</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">98</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">3</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">97</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">4</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">90</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">5</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">100</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">6</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">98</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">7</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">97</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                     <div class="scores">
-                        <h4 class="rank">8</h4>
-                        <h4 class="player">TupzZ</h4>
-                        <h4 class="score">90</h4>
+						<div class="column">
+							<h4 class="rank">8</h4>
+						</div>
+						<div class="column">
+							<h4 class="player">TupzZ</h4>
+						</div>
+						<div class="column">
+							<h4 class="score">90</h4>
+						</div>
                     </div>
                 </div>
             </div>
